994da9d174a1a80eba9ae61b063a5c37d7808a3b
Niels Basjes <github@basjes.nl>
2014-02-13 13:39:45 +0100

    Fixing the benchmarking Makefile

--- Benchmark/run.sh
+++ Benchmark/run.sh
@@ -3,12 +3,10 @@
 FILENAME=words-100K.txt.gz
 
 make ${FILENAME}
-FILESIZE=$(stat -c%s "$FILENAME")
 
 make wordcount-normal.txt
 
 for I in `seq 1 25` ; 
 do 
-    SPLITSIZE=$(echo "(${FILESIZE}/${I})+1" | bc)
-    make wordcount-${SPLITSIZE}.txt
+    make wordcount-${I}.txt
 done
